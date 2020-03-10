79d3f1fae2e67d32685df9d87e4d6dd6ca83901e
CarlosGo1994 <charls21ronald@gmail.com>
2020-03-10 14:21:01 -0600

    Added a loop for validation when the user intruduced zero or letters the program ask for the number again

--- js/script.js
+++ js/script.js
@@ -2,8 +2,16 @@
 
 /*this a program where you can introduce two numbers and it'll show which is greater and which is smaller*/
 
-let first_number = parseInt(prompt("Input the first Integer number",0));
-let second_number = parseInt(prompt("Input the second Integer number",0));
+var first_number = parseInt(prompt("Input the first Integer number",0));
+var second_number = parseInt(prompt("Input the second Integer number",0));
+
+while(first_number <= 0 || second_number <= 0 || isNaN(first_number) || isNaN(second_number))
+{
+     first_number = parseInt(prompt("Input the first Integer number",0));
+     second_number = parseInt(prompt("Input the second Integer number",0));
+
+}
+
 
 if(first_number > second_number)
 {
@@ -16,6 +24,10 @@ if(first_number > second_number)
  else if(first_number == second_number)
  {
      alert("The numbers are equals");
+ }
+ else
+ {
+    alert("Introduce correct numbers");
  }
 
 /*
